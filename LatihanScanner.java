fec4fd8102f9176f9416a993a42afb59b96a2eb5
unknown <erlandorico@yahoo.co.id>
2016-04-07 17:24:53 +0700

    oop

---
+++ LatihanScanner.java
@@ -0,0 +1,17 @@
+package latihanscanner;
+
+public class LatihanScanner {
+    public static void main(String[] args) {
+        int N;
+        char pil;
+       
+        Scanner input = new Scanner(System.in);
+        
+        N = input.nextInt();
+        pil = input.next().charAt(0);
+        
+        pil = input.nextInt().charAt(0);
+        
+    }
+    
+}
